be6818acde731942477f1ee701b64868e25c2bf2
msaafir <saafir57@gmail.com>
2025-08-11 15:55:47 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,8 @@
+
+let canvas = document.getElementById('gameCanvas');
+let ctx = canvas.getContext('2d');
+ctx.fillStyle = 'orange';
+ctx.fillRect(200,150,50,50);
+ctx.fillStyle = 'white';
+ctx.font = '20px sans-serif';
+ctx.fillText('Mask Bro’s Demo', 150, 100);
